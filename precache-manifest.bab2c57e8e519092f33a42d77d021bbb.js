60a5461c569b03fa7b7971afa98293d82b8866d7
Geert van der Ploeg <geert.van.der.ploeg@finalist.nl>
2019-12-08 21:12:28 +0100

    Updates

---
+++ precache-manifest.bab2c57e8e519092f33a42d77d021bbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f64dd6cc70a50e48f0964573e05725",
+    "url": "/preek-bingo/index.html"
+  },
+  {
+    "revision": "4cdfe475cd52aa50fccf",
+    "url": "/preek-bingo/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "ec80df8c9e804dc087f1",
+    "url": "/preek-bingo/static/css/main.01ec80bb.chunk.css"
+  },
+  {
+    "revision": "4cdfe475cd52aa50fccf",
+    "url": "/preek-bingo/static/js/2.61ea3f2c.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/preek-bingo/static/js/2.61ea3f2c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec80df8c9e804dc087f1",
+    "url": "/preek-bingo/static/js/main.4c61ba60.chunk.js"
+  },
+  {
+    "revision": "fbefd1cdd59de7b2794b",
+    "url": "/preek-bingo/static/js/runtime-main.e9edfc00.js"
+  }
+]);
